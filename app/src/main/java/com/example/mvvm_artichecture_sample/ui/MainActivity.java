e5073834ce4f203cab9a6e49cafbacc621b69a6f
Sharify <rezasharify1993.rsmi@gmail.com>
2019-12-08 13:51:08 +0330

    Check saveInstance null

--- app/src/main/java/com/example/mvvm_artichecture_sample/ui/MainActivity.java
+++ app/src/main/java/com/example/mvvm_artichecture_sample/ui/MainActivity.java
@@ -3,21 +3,20 @@ package com.example.mvvm_artichecture_sample.ui;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.example.mvvm_artichecture_sample.R;
 
 public class MainActivity extends AppCompatActivity {
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		MainFragment fragment = MainFragment.getInstance();
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		transaction.replace(R.id.main_container,fragment, "");
-		transaction.commit();
-	}
-	
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            MainFragment fragment = MainFragment.getInstance();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_container, fragment, MainFragment.TAG).commit();
+        }
+    }
 }
